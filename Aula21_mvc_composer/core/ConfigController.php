5c3c6fdc2d2bdfecfded3e9f6235df03907fa590
Caio Silva <bladellano@gmail.com>
2019-07-10 22:03:15 -0300

    Configurando o composer e core

---
+++ Aula21_mvc_composer/core/ConfigController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Core;
+
+/**
+ * Description of ConfigController
+ *
+ * @copyright (c) 2019, Caio Dellano - FireFly}
+ */
+class ConfigController {
+
+    private $Url;
+    private $UrlConjunto;
+    private $UrlController;
+    private $UrlMetodo;
+
+    public function __construct() {
+
+        if (!empty(filter_input(INPUT_GET, "url", FILTER_DEFAULT))) {
+
+            $this->Url = filter_input(INPUT_GET, "url", FILTER_DEFAULT);
+            $this->UrlConjunto = explode("/", $this->Url);
+
+            if (isset($this->UrlConjunto[0]) && (isset($this->UrlConjunto[1]))) {
+                $this->UrlController = $this->UrlConjunto[0];
+                $this->UrlMetodo = $this->UrlConjunto[1];
+            } else {
+
+                $this->UrlController = 'home';
+                $this->UrlMetodo = 'index';
+            }
+        } else {
+
+            $this->UrlController = 'home';
+            $this->UrlMetodo = 'index';
+        }
+        echo "Classe: {$this->UrlController} - Método: {$this->UrlMetodo}<br>";
+    }
+
+}
